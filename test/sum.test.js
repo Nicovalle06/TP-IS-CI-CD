35a5f669327cbee744e8ac5c794bb04fe1fc7dfb
Nico <alejandrovallejoszabala@gmail.com>
2025-06-11 00:37:15 -0300

    Test éxito

--- test/sum.test.js
+++ test/sum.test.js
@@ -11,35 +11,7 @@ function suma(a, b) {
 
 describe('Función suma', () => {
   it('debería sumar dos números', () => {
-    assert.strictEqual(suma(2, 4), 5); //Test fallo
+    assert.strictEqual(suma(2, 3), 5); //Test éxito
   });
 });
 
-
-// describe('POST /sumar', () => {
-//   it('debe devolver la suma de dos números', async () => {
-//     const respuesta = await request(app)
-//       .post('/sumar')
-//       .send({ num1: 5, num2: 7 });
-
-//     expect(respuesta.statusCode).toBe(200);
-//     expect(respuesta.body.resultado).toBe(12);
-//   });
-
-//   it('debe manejar strings numéricos correctamente', async () => {
-//     const respuesta = await request(app)
-//       .post('/sumar')
-//       .send({ num1: "3", num2: "2" });
-
-//     expect(respuesta.statusCode).toBe(200);
-//     expect(respuesta.body.resultado).toBe(5);
-//   });
-
-//   it('debe manejar números negativos', async () => {
-//     const respuesta = await request(app)
-//       .post('/sumar')
-//       .send({ num1: -4, num2: -6 });
-
-//     expect(respuesta.body.resultado).toBe(-10);
-//   });
-// });
