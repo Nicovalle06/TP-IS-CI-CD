7bb429880ff31c37d6ca3bcf85d57389e678d351
Nico <alejandrovallejoszabala@gmail.com>
2025-06-11 14:49:32 -0300

    Test éxito

--- test/sum.test.js
+++ test/sum.test.js
@@ -11,7 +11,7 @@ function suma(a, b) {
 
 describe('Función suma', () => {
   it('debería sumar dos números', () => {
-    assert.strictEqual(suma(2, 6), 5); //Test fallo
+    assert.strictEqual(suma(2, 3), 5); //Test éxito
   });
 });
 
